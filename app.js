1fdc72fd15f41290d0f6a8207ece58bf87a6b225
Ibrahim Baig <ibaig6990@gmail.com>
2024-12-23 16:39:43 +0500

    Update app.js

--- app.js
+++ app.js
@@ -61,6 +61,6 @@ io.on("connection", (socket) => {
   });
 });
 
-// Start server test
+// Start server test 2
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
